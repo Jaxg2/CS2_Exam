86f85b77584789e7dd389e9845da5bf64d286836
Jaxg2 <jaxon.gillham@gmail.com>
2024-07-17 10:29:56 -0500

    Dog constructor and getAge function.

--- Dog.cpp
+++ Dog.cpp
@@ -1,1 +1,30 @@
 #include "Dog.h"
+
+
+Dog::Dog()
+{
+	dog_age = 0;
+	hum_age = 0;
+}
+
+int Dog::getAge()
+{
+	
+	cout << "Enter the age of the dog in years: ";
+	cin >> dog_age;
+
+	hum_age = (dog_age * 7);
+
+	return hum_age;
+
+}
+
+string Dog::getName()
+{
+
+}
+
+string Dog::getBreed()
+{
+
+}
--- Dog.h
+++ Dog.h
@@ -1,5 +1,21 @@
-#pragma once
+#ifndef DOG_H
+#define DOG_H
+
+#include <iostream>
+using namespace std;
+
 class Dog
 {
+private:
+	int dog_age;
+	int hum_age;
+	string name;
+	string breed;
+public:
+	Dog();
+	int getAge();
+	string getName();
+	string getBreed();
 };
 
+#endif
